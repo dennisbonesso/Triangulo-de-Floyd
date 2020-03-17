e3b9925ad77f4ae1e001fc495b02d5009068d5c6
Dennis Bonesso <dennisbonesso@gmail.com>
2020-03-17 10:44:32 -0300

    envio do cs

---
+++ trianguloFloyd/trianguloFloyd/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace trianguloFloyd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coluna = 1;
+            int contador = 1;
+
+
+            while (contador<6) {
+                
+                while (coluna <= contador) {
+                    if (coluna % 2 == 0) {
+                        Console.Write("0");
+
+                    }
+                    if (coluna % 2 != 0) {
+                        Console.Write("1");
+
+                    }
+                    
+                    coluna++;
+
+
+
+                }
+                contador++;
+                coluna = 1;
+                Console.WriteLine("");
+            }
+            
+
+        }
+    }
+}
